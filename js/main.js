5e760d141795e10226ce9c943e4705106127d24c
Zach Denton <zacharydenton@gmail.com>
2013-07-23 23:11:36 -0400

    update site

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 (function() {
   $(function() {
     return $('h1, h2, h3, li, p').each(function() {
-      return $(this).html($(this).html().replace(/\s([\w.:-\(\)]+)$/, '&nbsp;$1'));
+      return $(this).html($(this).html().replace(/\s([\w.:\(\)\-]+)$/, '&nbsp;$1'));
     });
   });
 
